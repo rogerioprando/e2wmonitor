9dd98a7e6080154e8b5b61b989ad0bf089b23116
Rogerio <rogprando@gmail.com>
2018-02-18 23:56:27 -0300

    update js commit antes de mudar para blueprint e jinja template index em localhost:5000 OK

--- app/static/js/e2w.js
+++ app/static/js/e2w.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+
+  $('#submit-btn').on('click', function() {
+    $(this).addClass('disabled');
+    $('#cancel-btn').addClass('btn waves-effect waves-light red').removeClass('disabled');
+    //$('#loading-cmd').addClass('active');
+    $('#loading-cmd').show().addClass('active');
+  });
+
+  $('#cancel-btn').on('click', function() {
+    $(this).addClass('disabled');
+    $('#submit-btn').addClass('btn waves-effect waves-light').removeClass('disabled');
+    //$('#loading-cmd').removeClass('active').addClass('preloader-wrapper small');
+    $('#loading-cmd').hide();
+  })
+
+});
 
--- app/views/home.py
+++ app/views/home.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
 
-mod = Blueprint('home', __name__, url_prefix='/home', template_folder='templates')
+#mod = Blueprint('home', __name__, url_prefix='/', template_folder='templates')
 
+mod = Blueprint('index', __name__)
 
-@mod.route('/', defaults={'page': 'index'})
-@mod.route('/<page>')
-def index(page):
-        return render_template('home/%s.html' % page)
+@mod.route('/index', defaults={'page': 'index'})
+@mod.route('/')
+def index():
+    return render_template('index.html')
+
+#@mod.route('/', defaults={'page': 'index'})
+#@mod.route('/<page>')
+#def index(page):
+        #return render_template('/%s.html' % page)
+
